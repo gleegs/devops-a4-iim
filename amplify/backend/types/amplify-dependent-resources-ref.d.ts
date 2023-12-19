0fef7daf11b8c23753dc69ccb266186b33256bdc
maxquanticfy <maxime@quanticfy.io>
2023-12-19 10:22:03 +0100

    :sparkles: Add Auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,15 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "gleegsdevopsa487af3044": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "storage": {
     "gleegsDevopsDB": {
       "Arn": "string",
