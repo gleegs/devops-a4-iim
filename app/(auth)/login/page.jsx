572a6626360f6daba655b2d43bdee0af9c737202
ayman-benammour <benammourayman@gmail.com>
2023-12-20 11:43:43 +0100

    :sparkles: Add form ui

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,14 +1,8 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/GRCnMdpLzkK
- */
 "use client"
-import { FormLogin } from "@/components/component/form-login"
+import { FormLogin } from "/app/components/component/form-login"
 
 export default function Component() {
   return (
-    <div className="w-96">
       <FormLogin></FormLogin>
-    </div>
   )
 }
